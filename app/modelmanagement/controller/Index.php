19a1c2c407b8de051c099f586321dfff4f48aeab
zhy19712 <18055089@qq.com>
2018-05-17 21:44:28 +0800

    将所有模型页面按需装载到同一个iframe

--- app/modelmanagement/controller/Index.php
+++ app/modelmanagement/controller/Index.php
@@ -15,14 +15,24 @@ class Index extends Permissions
     function Index()
     {
         //将所有模型页面按需装载到同一个iframe中，防止用户打开过多模型页面崩溃
+        //全景3d、质量3d、进度3d、安全3d、全景模型、倾斜模型
         if(request()->isAjax()) {
             $id = $this->request->param('id');
             switch ($id) {
                 case 'panorama':
-                    $path = '../app/modelmanagement/view/index/panorama3d.html';
+                    $path = '../app/modelmanagement/view/index/panorama.html';
                     break;
-                case '3d':
-                    $path = '../app/modelmanagement/view/index/quality3d.html';
+                case 'quality':
+                    $path = '../app/modelmanagement/view/index/quality.html';
+                    break;
+                case 'progress':
+                    $path = '../app/modelmanagement/view/index/progress.html';
+                    break;
+                case 'safety':
+                    $path = '../app/modelmanagement/view/index/safety.html';
+                    break;
+                case 'oblique':
+                    $path = '../app/modelmanagement/view/index/oblique.html';
                     break;
                 case 'index':
                     $path = '../app/modelmanagement/view/index/index.html';
