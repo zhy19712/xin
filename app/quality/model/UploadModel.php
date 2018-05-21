734d20b2296b2f41cc6f1b069be7990d8a5e92ca
13522390376 <13522390376@163.com>
2018-05-21 15:22:03 +0800

    分部，单位工程修改

--- app/quality/model/UploadModel.php
+++ app/quality/model/UploadModel.php
@@ -25,7 +25,7 @@ class UploadModel extends Model
     public function insertTb($param)
     {
         try {
-            $result = $this->allowField(true)->insertAll($param);
+            $result = $this->allowField(true)->save($param);
             if (false === $result) {
                 return ['code' => -1, 'msg' => $this->getError()];
             } else {
