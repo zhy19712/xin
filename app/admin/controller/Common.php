33531d1c2d269160392c01d2320e8b3695775025
13522390376 <13522390376@163.com>
2018-05-23 14:54:56 +0800

    消息提醒

--- app/admin/controller/Common.php
+++ app/admin/controller/Common.php
@@ -369,7 +369,6 @@ class Common extends Controller
             if($limitFlag){
                 //*****多表查询join改这里******
                 $recordsFilteredResult = Db::name($table)->alias("m")
-                    ->join('quality_form_info f','m.uint_id = f.id','left')
                     ->join('admin a','m.sender = a.id','left')
                     ->field("m.task_name,m.create_time,a.nickname,m.task_category,m.status")
                     ->where($columnString, 'like', '%' . $search . '%')
@@ -381,7 +380,6 @@ class Common extends Controller
             //没有搜索条件的情况
             if($limitFlag){
                 $recordsFilteredResult = Db::name($table)->alias("m")
-                    ->join('quality_form_info f','m.uint_id = f.id','left')
                     ->join('admin a','m.sender = a.id','left')
                     ->field("m.task_name,m.create_time,a.nickname,m.task_category,m.status")
                     ->order($order)->limit(intval($start),intval($length))
