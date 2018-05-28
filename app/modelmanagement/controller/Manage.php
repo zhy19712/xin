62ab30e5740dc0bb9c0444169612560517dcafc8
wyang <wyang1117@msn.com>
2018-05-28 22:18:02 +0800

    完成模型节点联动显隐

--- app/modelmanagement/controller/Manage.php
+++ app/modelmanagement/controller/Manage.php
@@ -7,6 +7,7 @@
  */
 
 namespace app\modelmanagement\controller;
+
 use app\admin\controller\Permissions;
 use app\modelmanagement\model\QualitymassModel;
 use think\Db;
@@ -26,14 +27,14 @@ class Manage extends Permissions
     {
         if ($this->request->isAjax()) {
             //实例化模型类
-            $model =  new QualitymassModel();
+            $model = new QualitymassModel();
 
-            $model_id = input('post.model_id')?input('post.model_id'):"";
+            $model_id = input('post.model_id') ? input('post.model_id') : "";
 //            $model_id = 5;
             /*******基本信息**********/
             $unit_info = $model->getUnitInfo($model_id);
 
-            $unit_info = empty($unit_info)?[]:$unit_info;
+            $unit_info = empty($unit_info) ? [] : $unit_info;
 
             //获取xin_quality_unit表中的工程类型，en_type为xin_norm_materialtrackingdivision质量验评标准库-工序中的pid
             $en_type = $unit_info["en_type"];
@@ -42,21 +43,19 @@ class Manage extends Permissions
             $processinfo = $model->getProcessInfo($en_type);
 
             //需要的条件 unit_id->division_id checked type=1
-            if(!empty($processinfo))
-            {
-                foreach ($processinfo as $key=>$val)
-                {
+            if (!empty($processinfo)) {
+                foreach ($processinfo as $key => $val) {
                     //查询控制点的信息
-                    $processinfo[$key]["controlpoint_info"]=
+                    $processinfo[$key]["controlpoint_info"] =
                         Db::name('norm_controlpoint')->alias('c')
-                        ->where("c.procedureid",$val["id"])
-                        ->field("c.id,c.procedureid,c.code,c.name,c.qualitytemplateid")
-                        ->order('c.code asc')
-                        ->select();
+                            ->where("c.procedureid", $val["id"])
+                            ->field("c.id,c.procedureid,c.code,c.name,c.qualitytemplateid")
+                            ->order('c.code asc')
+                            ->select();
 
-                    $processinfo[$key]["unit_id"]=$unit_info["id"];//unit表中的主键id
+                    $processinfo[$key]["unit_id"] = $unit_info["id"];//unit表中的主键id
                 }
-                return json(["code"=>1,"processinfo"=>$processinfo]);
+                return json(["code" => 1, "processinfo" => $processinfo]);
 
             }
         }
@@ -76,41 +75,41 @@ class Manage extends Permissions
             $DivisionId = input("unit_id");
 //            $DivisionId = 17;
             //当前登录用户id
-            $admin_id= Session::has('admin') ? Session::get('admin') : 0;
+            $admin_id = Session::has('admin') ? Session::get('admin') : 0;
 
             //查询xin_quality_form_info在线填报表的中的信息
-        $form_info = Db::name("quality_form_info")->alias("q")
-            ->join('admin ad', 'ad.id=q.user_id', 'left')
-            ->field("ad.nickname,FROM_UNIXTIME(q.update_time,'%Y-%c-%d') as update_time,q.ApproveStatus,q.id,q.user_id,q.CurrentApproverId,q.CurrentStep")
-            ->where(["DivisionId"=>$DivisionId,"ProcedureId"=>$ProcedureId,"ControlPointId"=>$ControlPointId])
-            ->order("update_time desc")
-            ->select();
+            $form_info = Db::name("quality_form_info")->alias("q")
+                ->join('admin ad', 'ad.id=q.user_id', 'left')
+                ->field("ad.nickname,FROM_UNIXTIME(q.update_time,'%Y-%c-%d') as update_time,q.ApproveStatus,q.id,q.user_id,q.CurrentApproverId,q.CurrentStep")
+                ->where(["DivisionId" => $DivisionId, "ProcedureId" => $ProcedureId, "ControlPointId" => $ControlPointId])
+                ->order("update_time desc")
+                ->select();
 
             //查询xin_quality_division_controlpoint_relation对应关系表
-        $relation_form = Db::name("quality_division_controlpoint_relation")
-            ->field("id")
-            ->where(["division_id"=>$DivisionId,"ma_division_id"=>$ProcedureId,"control_id"=>$ControlPointId])
-            ->where(["type"=>1,"checked"=>0])
-            ->find();
+            $relation_form = Db::name("quality_division_controlpoint_relation")
+                ->field("id")
+                ->where(["division_id" => $DivisionId, "ma_division_id" => $ProcedureId, "control_id" => $ControlPointId])
+                ->where(["type" => 1, "checked" => 0])
+                ->find();
 
             //查询xin_quality_upload表中的扫描件和附件资料
-        $upload_form_sao = Db::name("quality_upload")->alias("u")
-            ->join('attachment a', 'a.id=u.attachment_id', 'left')
-            ->join('admin ad', 'ad.id=a.user_id', 'left')
-            ->field("u.data_name,ad.nickname,FROM_UNIXTIME(a.create_time,'%Y-%c-%d') as create_time,u.id,u.type")
-            ->where("u.contr_relation_id",$relation_form["id"])
-            ->where("type = 1")
-            ->select();
-
-        $upload_form_fu = Db::name("quality_upload")->alias("u")
+            $upload_form_sao = Db::name("quality_upload")->alias("u")
                 ->join('attachment a', 'a.id=u.attachment_id', 'left')
                 ->join('admin ad', 'ad.id=a.user_id', 'left')
                 ->field("u.data_name,ad.nickname,FROM_UNIXTIME(a.create_time,'%Y-%c-%d') as create_time,u.id,u.type")
-                ->where("u.contr_relation_id",$relation_form["id"])
+                ->where("u.contr_relation_id", $relation_form["id"])
+                ->where("type = 1")
+                ->select();
+
+            $upload_form_fu = Db::name("quality_upload")->alias("u")
+                ->join('attachment a', 'a.id=u.attachment_id', 'left')
+                ->join('admin ad', 'ad.id=a.user_id', 'left')
+                ->field("u.data_name,ad.nickname,FROM_UNIXTIME(a.create_time,'%Y-%c-%d') as create_time,u.id,u.type")
+                ->where("u.contr_relation_id", $relation_form["id"])
                 ->where("type = 4")
                 ->select();
             //form_info,在线填报，relation_form，relation关系表中的主键id,upload_form,$upload_form_sao扫描件,$upload_form_fu附件
-            return json(["code"=>1,"admin_id"=>$admin_id,"form_info"=>$form_info,"relation_form"=>$relation_form,"upload_form_sao"=>$upload_form_sao,"upload_form_fu"=>$upload_form_fu]);
+            return json(["code" => 1, "admin_id" => $admin_id, "form_info" => $form_info, "relation_form" => $relation_form, "upload_form_sao" => $upload_form_sao, "upload_form_fu" => $upload_form_fu]);
         }
     }
 
@@ -122,11 +121,11 @@ class Manage extends Permissions
     {
         if ($this->request->isAjax()) {
             //实例化模型类
-            $model =  new QualitymassModel();
-            $model_id = input('post.model_id')?input('post.model_id'):"";
+            $model = new QualitymassModel();
+            $model_id = input('post.model_id') ? input('post.model_id') : "";
 //            $model_id = 13;
             $attribute = $model->getUnitInfo($model_id);
-            return json(["code"=>1,"attribute"=>$attribute]);
+            return json(["code" => 1, "attribute" => $attribute]);
         }
     }
 }
