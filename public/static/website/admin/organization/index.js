d37c5a29559639f71ef0977ce02e02f5e2ca150d
wyang <wyang1117@msn.com>
2018-06-01 16:38:08 +0800

    修改组织机构页面样式

--- public/static/website/admin/organization/index.js
+++ public/static/website/admin/organization/index.js
@@ -404,6 +404,8 @@
         ajax: {
             "url":"/admin/common/datatablesPre?tableName=admin"
         },
+        dom:'lf<"#add.add layui-btn layui-btn-normal btn-right">rtip',
+        isPage:false,
         columns:[
             {
                 name: "g_order"
@@ -451,9 +453,7 @@
         ]
     });
 
-    $('#tableItem tbody').on( 'click', 'tr', function () {
-        $(this).toggleClass('active').siblings().removeClass('active');
-    } );
+    $('#add').html('新增');
 
     //新增上传电子签名
     uploader = WebUploader.create({
