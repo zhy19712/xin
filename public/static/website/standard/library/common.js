e8296b371f0174d84dcdf17d67b44387bd444140
dingzicha <15893001150@163.com>
2018-06-05 10:59:46 +0800

    改动

--- public/static/website/standard/library/common.js
+++ public/static/website/standard/library/common.js
@@ -5,7 +5,7 @@ var tableItem = $('#tableItem').DataTable({
   processing: true,
   serverSide: true,
   ajax: {
-    "url": "../common/datatablesPre.shtml?tableName=norm_controlpoint&id="
+    "url": "../common/datatablesPre.shtml?tableName=norm_controlpoint&id=-1"
   },
   dom: 'lf<"mybtn layui-btn layui-btn-sm">rtip',
   columns: [
@@ -131,18 +131,14 @@ function nodeClick(e, treeId, node) {
   if(sNodes.level != 0){
     if(sNodes.level != 1){
       tableItem.ajax.url("../common/datatablesPre.shtml?tableName=norm_controlpoint&id="+selfid).load();
-      $("#tableContent .dataTables_wrapper").css('display','block');
-      $("#tableContent .tbcontainer").css('display','block');
+
     }else if(sNodes.level == 1){
-      $("#tableContent .dataTables_wrapper").css('display','none');
-      $("#tableContent .tbcontainer").css('display','none');
+      tableItem.ajax.url("../common/datatablesPre.shtml?tableName=norm_controlpoint&id=-1").load();
     }
     $(".layout-panel-center .panel-title").text("当前路径:" + path);
   }else if(sNodes.level == 0){
-    tableItem.ajax.url("../common/datatablesPre.shtml?tableName=norm_controlpoint&id=").load();
-    $("#tableContent .dataTables_wrapper").css('display','none');
-    $("#tableContent .tbcontainer").css('display','none');
-    $(".layout-panel-center .panel-title").text("");
+    tableItem.ajax.url("../common/datatablesPre.shtml?tableName=norm_controlpoint&id=-1").load();
+    $(".layout-panel-center .panel-title").text("当前路径:"+ path);
   }
   procedureId = selfid;
 }
