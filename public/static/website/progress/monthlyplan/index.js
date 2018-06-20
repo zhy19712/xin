57d641b662db30483907ca84b19d9031853a4d42
wjw <save_lifeme@163.com>
2018-06-20 15:00:20 +0800

    月计划管理-新增

--- public/static/website/progress/monthlyplan/index.js
+++ public/static/website/progress/monthlyplan/index.js
@@ -173,42 +173,7 @@ function monthlyPlanList() {
 
 //1)自定义条形图外观显示
 /*
-project.on("drawitem", function (e) {
-    var item = e.item;
-    var left = e.itemBox.left,
-        top = e.itemBox.top,
-        width = e.itemBox.width,
-        height = e.itemBox.height;
 
-    if (!item.Summary && !item.Milestone) {
-        var percentWidth = width * (item.PercentComplete / 100);
-
-        e.itemHtml = '<div id="' + item._id + '" class="myitem" style="left:' + left + 'px;top:' + top + 'px;width:' + width + 'px;height:' + (height) + 'px;">';
-        e.itemHtml += '<div style="width:' + (percentWidth) + 'px;" class="percentcomplete"></div>';
-
-        //根据你自己逻辑，把任务分成几块，注意坐标和宽度
-        // e.itemHtml += '<div style="position:absolute;left:0px;top:0;height:100%;width:20px;background:red;"></div>';
-
-        e.itemHtml += '</div>';
-
-        // e.ItemHtml = '<a href="http://www.baidu.com" style="left:'+left+'px;top:'+top+'px;width:'+width+'px;height:'+(height-2)+'px;" class="myitem">111</a>';
-    }
-});
-
-
-project.on("drawcell", function (e) {
-    var task = e.record, column = e.column, field = e.field;
-
-    //新增
-    if (task._state == "added") {
-        e.rowCls = "row_added";
-    }
-    //删除
-    if (task.Deleted == true) {
-        e.rowCls = "row_deleted";
-    }
-
-});
 //2)保存
 function save() {
     var newTask = project.newTask();
@@ -290,37 +255,4 @@ function removeTask() {
         alert("请选中任务");
     }
 }
-//10)更新
-function updateTask() {
-    var selectedTask = project.getSelected();
-    alert("编辑选中任务:" + selectedTask.Name);
-}
-//11)升级
-function upgradeTask() {
-    var task = project.getSelected();
-    if (task) {
-        project.upgradeTask(task);
-    } else {
-        alert("请选选中任务");
-    }
-}
-//12)降级
-function downgradeTask() {
-    var task = project.getSelected();
-    if (task) {
-        project.downgradeTask(task);
-    } else {
-        alert("请选选中任务");
-    }
-}
-
-//13)锁定列
-function frozenColumn() {
-    project.frozenColumn(0, 1);
-}
-//14)打印
-function printGantt() {
-    project.printServer = "__PUBLIC__/gantts/scripts/plusproject/services/snapshot/snapshot.aspx";
-    project.printCSS = "__PUBLIC__/gantts/scripts/miniui/themes/default/miniui.css";
-    project.print();
-}*/
+*/
